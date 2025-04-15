697db7b74a1a533accaaf585a526f729173c2275
danhdot <your-email@example.com>
2025-04-15 09:19:14 +0700

    adjust

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
   server: {
     port: 3000,
     proxy: {
@@ -19,7 +14,9 @@ export default defineConfig({
       }
     }
   },
-  define: {
-    'process.env': {}
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
-});
+})
